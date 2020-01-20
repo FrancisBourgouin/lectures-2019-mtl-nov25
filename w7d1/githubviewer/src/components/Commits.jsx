7990688b0ebb804b79be6f5952d53008de90a268
Francis Bourgouin <netbeuix@gmail.com>
2020-01-20 14:01:56 -0500

    added fancy numbers

--- w7d1/githubviewer/src/components/Commits.jsx
+++ w7d1/githubviewer/src/components/Commits.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import SingularCommit from "./SingularCommit";
 
 const Commits = ({ commits }) => {
-  const listOfCommits = commits.map(commit => (
+  const listOfCommits = commits.map((commit, index) => (
     <SingularCommit
       name={commit.author.login}
       message={commit.commit.message}
       date={commit.commit.author.date}
+      key={index}
+      index={index}
     />
   ));
   return (
--- w7d1/githubviewer/src/components/SingularCommit.jsx
+++ w7d1/githubviewer/src/components/SingularCommit.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-const SingularCommit = ({ name, message, date }) => {
+const SingularCommit = ({ name, message, date, index }) => {
   return (
     <article className='commit'>
       <h1>{name}</h1>
       <h2>{message}</h2>
       <h3>{date}</h3>
+      <i>{index}</i>
     </article>
   );
 };
